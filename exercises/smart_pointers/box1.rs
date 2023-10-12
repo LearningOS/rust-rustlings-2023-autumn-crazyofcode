f87619235bdeeb405bb892e68fc143644d45252b
crazyofcode <1710338943@qq.com>
2023-10-12 20:31:44 +0800

    update

--- exercises/smart_pointers/box1.rs
+++ exercises/smart_pointers/box1.rs
@@ -18,11 +18,10 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -34,12 +33,16 @@ fn main() {
     );
 }
 
+use List::Cons;
+use List::Nil;
 pub fn create_empty_list() -> List {
-    todo!()
+    let list = List::Nil;
+    list
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    let list = Cons(1,Box::new(Nil));
+    list
 }
 
 #[cfg(test)]
--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -27,6 +27,12 @@ fn main() {
     let mut results: Vec<u128> = vec![];
     for handle in handles {
         // TODO: a struct is returned from thread::spawn, can you use it?
+        thread::spawn(move || {
+            let join_handle: thread::JoinHandle<_> = builder.spawn(|| {
+                join_handle.push(handle);
+            }).unwarp();
+            join_handle
+        })
     }
 
     if results.len() != 10 {
